0768b2fe62b2ff58062a13632cfb019ef0d565e0
GoodLGames <88364745+FuneralPyreGames@users.noreply.github.com>
2021-11-21 15:04:24 -0500

    yee

--- Assets/Scripts/Persistent/Audio.cs
+++ Assets/Scripts/Persistent/Audio.cs
@@ -6,6 +6,7 @@ public class Audio : MonoBehaviour
 {
     [SerializeField] AudioClip pilesOfSnow;
     [SerializeField] AudioClip vehicularCombat;
+    [SerializeField] AudioClip Stardust;
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
