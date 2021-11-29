fd8a8de13c704db6ea79ec91f0f2c737e7c9aa60
GoodLGames <88364745+FuneralPyreGames@users.noreply.github.com>
2021-11-29 02:44:59 -0500

    work baby

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -11,6 +11,13 @@ public class MainMenu : MonoBehaviour
     public GameObject optionsMenu;
     public GameObject levelSelectNotUnlocked;
     public GameObject creditsMenu;
+    public GameObject howToPlay;
+    public GameObject howToPlay1;
+    public GameObject howToPlay2;
+    public GameObject howToPlay3;
+    public GameObject howToPlay4;
+    public GameObject howToPlay5;
+    public GameObject howToPlay6;
     public PersistentData persistentData;
     public SceneChangeManager sceneChangeManager;
     public SaveManager saveManager;
@@ -82,5 +89,45 @@ public class MainMenu : MonoBehaviour
     public void CloseCredits()
     {
         creditsMenu.SetActive(false);
+    }
+    public void LoadHowToPlay1()
+    {
+        howToPlay.SetActive(true);
+        howToPlay1.SetActive(true);
+    }
+    public void LoadHowToPlay2()
+    {
+        howToPlay1.SetActive(false);
+        howToPlay2.SetActive(true);
+    }
+    public void LoadHowToPlay3()
+    {
+        howToPlay2.SetActive(false);
+        howToPlay3.SetActive(true);
+    }
+    public void LoadHowToPlay4()
+    {
+        howToPlay3.SetActive(false);
+        howToPlay4.SetActive(true);
+    }
+    public void LoadHowToPlay5()
+    {
+        howToPlay4.SetActive(false);
+        howToPlay5.SetActive(true);
+    }
+    public void LoadHowToPlay6()
+    {
+        howToPlay5.SetActive(false);
+        howToPlay6.SetActive(true);
+    }
+    public void CloseHowToPlay()
+    {
+        howToPlay1.SetActive(false);
+        howToPlay2.SetActive(false);
+        howToPlay3.SetActive(false);
+        howToPlay4.SetActive(false);
+        howToPlay5.SetActive(false);
+        howToPlay6.SetActive(false);
+        howToPlay.SetActive(false);
     }
 }
